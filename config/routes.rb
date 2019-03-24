ec2b55c6c18af4bfe41ea9ff2b308dbb04717b5f
Peter Klassen <klassen@hiskp.uni-bonn.de>
2019-03-24 17:34:20 +0100

    name routes to static pages and use them in header/footer links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get "/contact", to: 'static_pages#contact'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get "/home",    to: 'static_pages#home'
+  get "/help",    to: 'static_pages#help'
+  get "/about",   to: 'static_pages#about'
   get 'users/index'
   get 'users/new'
   get 'users/show'
