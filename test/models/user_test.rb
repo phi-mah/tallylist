532f2b99910f339b8001164bc02356a0c1f61316
Peter Klassen <klassen@hiskp.uni-bonn.de>
2019-03-29 10:47:50 +0100

    test user validity with given userID

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new userID: "AF45EF3"
+  end
+
+  test "not empty uiserID should be valid" do
+    assert @user.valid?
+  end
   # test "the truth" do
   #   assert true
   # end
