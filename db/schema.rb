69b4e369815e23719fd2e994920f452d56ce31ca
Peter Klassen <klassen@hiskp.uni-bonn.de>
2019-03-29 21:06:11 +0100

    admin password field

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_101046) do
+ActiveRecord::Schema.define(version: 2019_03_29_195903) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "tallies", force: :cascade do |t|
