3be9402aacc63376384f4968f0fb48622e3c9c64
Peter Klassen <klassen@hiskp.uni-bonn.de>
2019-04-04 00:15:17 +0200

    Add Tally via new action and rerender the user show action

--- app/controllers/tallies_controller.rb
+++ app/controllers/tallies_controller.rb
@@ -1,2 +1,9 @@
 class TalliesController < ApplicationController
+
+  def new
+    u = User.find_by(userID: params[:userID])
+    u.tallies.create
+
+    redirect_to :controller => 'users', :action => 'show', :userID => u.userID
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/:userID', to: 'users#show'
 
+  post "tallies",  to: 'tallies#new'
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
